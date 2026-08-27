381963bcabd0d8260b09ff90e7e474f30f21edf0
Isac181004 <isacvasquez017@gmail.com>
2026-08-27 17:48:12 -0500

    fix: wrap long client data on 58mm tickets

--- resources/views/pdf/components/client-info-ticket-exact.blade.php
+++ resources/views/pdf/components/client-info-ticket-exact.blade.php
@@ -5,12 +5,24 @@
 @endphp
 
 <div class="client-section">
-    <div class="client-name">CLIENTE: {{ strtoupper($client['razon_social'] ?? 'CLIENTE') }}</div>
-    <div class="client-details"><strong>{{ $etiquetaDoc }}:</strong> {{ $client['numero_documento'] ?? '' }}</div>
+    <div class="client-name">
+        <strong>CLIENTE:</strong><br>
+        {{ strtoupper($client['razon_social'] ?? 'CLIENTE') }}
+    </div>
+
+    <div class="client-details">
+        <strong>{{ $etiquetaDoc }}:</strong> {{ $client['numero_documento'] ?? '' }}
+    </div>
 
     @if($direccion !== '')
-        <div class="client-details"><strong>DIRECCIÓN:</strong> {{ strtoupper($direccion) }}</div>
+        <div class="client-details">
+            <strong>DIRECCIÓN:</strong><br>
+            {{ strtoupper($direccion) }}
+        </div>
     @endif
 
-    <div class="client-details"><strong>FECHA:</strong> {{ $fecha_emision ?? now()->format('d/m/Y') }} &nbsp; <strong>HORA:</strong> {{ now()->format('H:i') }}</div>
+    <div class="client-details">
+        <strong>FECHA:</strong> {{ $fecha_emision ?? now()->format('d/m/Y') }}<br>
+        <strong>HORA:</strong> {{ now()->format('H:i') }}
+    </div>
 </div>
