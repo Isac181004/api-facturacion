e0c0d6799f771f45ef17d6633e5bca2d935af9a0
Isac181004 <isacvasquez017@gmail.com>
2026-08-27 17:06:56 -0500

    feat: display unit names and IGV status in A4 receipt items

--- resources/views/pdf/components/items-table.blade.php
+++ resources/views/pdf/components/items-table.blade.php
@@ -1,12 +1,11 @@
 {{-- PDF Items Table Component --}}
 {{-- Props: $detalles, $format --}}
 @php
-     $maxFilas = in_array($format, ['a5', 'A5']) ? 8 : 15;
+    $maxFilas = in_array($format, ['a5', 'A5']) ? 8 : 15;
     $contador = count($detalles);
 @endphp
 
 @if(in_array($format, ['a4', 'A4', 'a5', 'A5']))
-    {{-- A4/A5 Items Table --}}
     <table class="items-table">
         <thead>
             <tr>
@@ -15,40 +14,52 @@
                 <th>DESCRIPCIÓN</th>
                 <th>UNIDAD</th>
                 <th>CANT.</th>
+                <th>IGV</th>
                 <th>P. UNIT.</th>
                 <th>TOTAL</th>
             </tr>
         </thead>
         <tbody>
-            {{-- Items reales --}}
             @foreach($detalles as $index => $detalle)
+                @php
+                    $unidadCodigo = strtoupper((string)($detalle['unidad'] ?? ''));
+                    $unidadVisible = match($unidadCodigo) {
+                        'ZZ' => 'SERVICIO',
+                        'NIU' => 'UNIDAD',
+                        default => $unidadCodigo ?: 'UNIDAD'
+                    };
+                    $tipAfe = (string)($detalle['tip_afe_igv'] ?? '10');
+                    $igvTexto = match($tipAfe) {
+                        '10' => '18%',
+                        '20' => 'EXON.',
+                        '30' => 'INAF.',
+                        default => number_format((float)($detalle['porcentaje_igv'] ?? 0), 0) . '%'
+                    };
+                    $valorVenta = (float)($detalle['mto_valor_venta'] ?? 0);
+                    $impuestos = (float)($detalle['total_impuestos'] ?? ($detalle['igv'] ?? 0));
+                    $precioFinalUnitario = (float)($detalle['mto_precio_unitario'] ?? ($detalle['mto_valor_unitario'] ?? 0));
+                    $totalFinalLinea = $valorVenta + $impuestos;
+                @endphp
                 <tr>
                     <td>{{ $index + 1 }}</td>
                     <td>{{ $detalle['codigo'] ?? '' }}</td>
                     <td>{{ $detalle['descripcion'] ?? '' }}</td>
-                    <td>{{ $detalle['unidad'] ?? 'NIU' }}</td>
+                    <td>{{ $unidadVisible }}</td>
                     <td>{{ number_format($detalle['cantidad'] ?? 0, 2) }}</td>
-                    <td>{{ number_format($detalle['mto_valor_unitario'] ?? 0, 2) }}</td>
-                    <td>{{ number_format($detalle['mto_valor_venta'] ?? 0, 2) }}</td>
+                    <td>{{ $igvTexto }}</td>
+                    <td>{{ number_format($precioFinalUnitario, 2) }}</td>
+                    <td>{{ number_format($totalFinalLinea, 2) }}</td>
                 </tr>
             @endforeach
 
-            {{-- Filas vacías --}}
             @for($i = $contador; $i < $maxFilas; $i++)
                 <tr>
-                    <td>&nbsp;</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>&nbsp;</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
                 </tr>
             @endfor
         </tbody>
     </table>
 @else
-    {{-- Ticket Items Table --}}
     <table class="items-table">
         <thead>
             <tr>
@@ -61,12 +72,17 @@
         </thead>
         <tbody>
             @foreach($detalles as $detalle)
+                @php
+                    $valorVenta = (float)($detalle['mto_valor_venta'] ?? 0);
+                    $impuestos = (float)($detalle['total_impuestos'] ?? ($detalle['igv'] ?? 0));
+                    $precioFinalUnitario = (float)($detalle['mto_precio_unitario'] ?? ($detalle['mto_valor_unitario'] ?? 0));
+                @endphp
                 <tr>
                     <td class="text-center">{{ $detalle['codigo'] ?? '-' }}</td>
                     <td class="text-left">{{ Str::limit($detalle['descripcion'] ?? '', 20) }}</td>
                     <td class="text-center">{{ number_format($detalle['cantidad'] ?? 0, 2) }}</td>
-                    <td class="text-right">{{ number_format($detalle['mto_valor_unitario'] ?? 0, 2) }}</td>
-                    <td class="text-right">{{ number_format($detalle['mto_precio_unitario'] ?? $detalle['mto_valor_unitario'] ?? 0, 2) }}</td>
+                    <td class="text-right">{{ number_format($precioFinalUnitario, 2) }}</td>
+                    <td class="text-right">{{ number_format($valorVenta + $impuestos, 2) }}</td>
                 </tr>
             @endforeach
         </tbody>
