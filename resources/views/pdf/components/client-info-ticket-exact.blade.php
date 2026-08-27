281f8bef165f014a5b75c8e0f2b489945b36e410
Isac181004 <isacvasquez017@gmail.com>
2026-08-27 13:03:58 -0500

    fix: show DNI or RUC correctly on thermal receipts

--- resources/views/pdf/components/client-info-ticket-exact.blade.php
+++ resources/views/pdf/components/client-info-ticket-exact.blade.php
@@ -1,18 +1,16 @@
-{{-- PDF Ticket Client Info Component (Exact Design Match) --}}
-{{-- Props: $client, $fecha_emision --}}
+@php
+    $tipoDoc = (string)($client['tipo_documento'] ?? '1');
+    $etiquetaDoc = $tipoDoc === '6' ? 'RUC' : ($tipoDoc === '1' ? 'DNI' : 'DOC');
+    $direccion = trim((string)($client['direccion'] ?? ''));
+@endphp
 
 <div class="client-section">
-    {{-- Client Name --}}
-    <div class="client-name">{{ strtoupper($client['razon_social'] ?? 'CAMILO SANCHEZ') }}</div>
-    
-    {{-- Separator --}}
-    <div class="client-separator">---</div>
-    
-    {{-- Document Number --}}
-    <div class="client-details">DNI {{ $client['numero_documento'] ?? '71262017' }}</div>
-    
-    {{-- Date and Time --}}
-    <div class="client-details">
-        FECHA: {{ $fecha_emision ?? '06/03/2024' }} HORA: {{ now()->format('H:i:s A') }}
-    </div>
+    <div class="client-name">CLIENTE: {{ strtoupper($client['razon_social'] ?? 'CLIENTE') }}</div>
+    <div class="client-details"><strong>{{ $etiquetaDoc }}:</strong> {{ $client['numero_documento'] ?? '' }}</div>
+
+    @if($direccion !== '')
+        <div class="client-details"><strong>DIRECCIÓN:</strong> {{ strtoupper($direccion) }}</div>
+    @endif
+
+    <div class="client-details"><strong>FECHA:</strong> {{ $fecha_emision ?? now()->format('d/m/Y') }} &nbsp; <strong>HORA:</strong> {{ now()->format('H:i') }}</div>
 </div>
