6d5d934b42b11351a046436d5857374c951a8690
Isac181004 <isacvasquez017@gmail.com>
2026-08-27 12:49:32 -0500

    feat: add unified manual receipt route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/comprobantes/manual', function () {
+    return view('boletas.manual');
+})->name('comprobantes.manual');
+
 Route::get('/boletas/manual', function () {
     return view('boletas.manual');
 })->name('boletas.manual');
