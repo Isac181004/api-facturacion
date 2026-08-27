e7dd39af3d525f808c7c87f61936c66fccb64fdd
Isac181004 <isacvasquez017@gmail.com>
2026-08-27 18:23:08 -0500

    fix: support preload token in comprobante route path

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@ Route::get('/comprobantes/manual', function () {
     return view('boletas.manual-integration');
 })->name('comprobantes.manual');
 
+Route::get('/comprobantes/manual/{preload}', function (string $preload) {
+    return view('boletas.manual-integration');
+})->where('preload', '[A-Za-z0-9]{48}')->name('comprobantes.manual.preload');
+
 Route::get('/boletas/manual', function () {
     return view('boletas.manual');
 })->name('boletas.manual');
