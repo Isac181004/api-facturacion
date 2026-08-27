084a642bb0f770fc1a18ce56aa03fb70443ed7ad
Isac181004 <isacvasquez017@gmail.com>
2026-08-27 17:06:29 -0500

    feat: show clear unit and IGV status per ticket item

--- resources/views/pdf/components/items-table-ticket-exact.blade.php
+++ resources/views/pdf/components/items-table-ticket-exact.blade.php
@@ -1,32 +1,51 @@
-{{-- PDF Ticket Items Component (Exact Design Match) --}}
-{{-- Props: $detalles --}}
-
-{{-- Items Header --}}
 <div class="items-header">
-    <div class="header-cant">Cant</div>
-    <div class="header-um">U.M</div>
-    <div class="header-cod">COD</div>
-    <div class="header-precio">PRECIO</div>
+    <div class="header-cant">CANT</div>
+    <div class="header-um">U.M.</div>
+    <div class="header-cod">CÓD.</div>
+    <div class="header-precio">P.UNIT</div>
     <div class="header-total">TOTAL</div>
 </div>
-<div class="items-header" style="border-top: none; border-bottom: none; font-weight: bold; padding: 1px 0;">
-    <div style="width: 100%; text-align: left; padding-left: 5px;">DESCRIPCION</div>
+<div class="items-header" style="border-top:none;border-bottom:none;font-weight:bold;padding:1px 0;">
+    <div style="width:100%;text-align:left;padding-left:1px;">DESCRIPCIÓN / AFECTACIÓN</div>
 </div>
 
-{{-- Items List --}}
 <div class="items-section">
-    @forelse($detalles as $index => $detalle)
+    @forelse($detalles as $detalle)
+        @php
+            $unidadCodigo = strtoupper((string)($detalle['unidad'] ?? ''));
+            $unidadVisible = match($unidadCodigo) {
+                'ZZ' => 'SERV.',
+                'NIU' => 'UND.',
+                default => $unidadCodigo ?: 'UND.'
+            };
+
+            $tipAfe = (string)($detalle['tip_afe_igv'] ?? '10');
+            $igvTexto = match($tipAfe) {
+                '10' => 'IGV: SÍ · 18% GRAVADO',
+                '20' => 'IGV: NO · EXONERADO',
+                '30' => 'IGV: NO · INAFECTO',
+                default => 'IGV: ' . number_format((float)($detalle['porcentaje_igv'] ?? 0), 0) . '%'
+            };
+
+            $cantidad = (float)($detalle['cantidad'] ?? 1);
+            $valorVenta = (float)($detalle['mto_valor_venta'] ?? 0);
+            $impuestos = (float)($detalle['total_impuestos'] ?? ($detalle['igv'] ?? 0));
+            $precioFinalUnitario = (float)($detalle['mto_precio_unitario'] ?? ($detalle['mto_valor_unitario'] ?? 0));
+            $totalFinalLinea = $valorVenta + $impuestos;
+        @endphp
+
         <div class="item">
-            <div class="item-cant">{{ number_format($detalle['cantidad'] ?? 1, 0) }}</div>
-            <div class="item-um">{{ $detalle['unidad'] ?? 'UNIDAD' }}</div>
-            <div class="item-cod">{{ $detalle['codigo'] ?? '9810007005004' }}</div>
-            <div class="item-precio">{{ number_format($detalle['mto_valor_unitario'] ?? 20.00, 2) }}</div>
-            <div class="item-total">{{ number_format($detalle['mto_valor_venta'] ?? 20.00, 2) }}</div>
+            <div class="item-cant">{{ number_format($cantidad, 0) }}</div>
+            <div class="item-um">{{ $unidadVisible }}</div>
+            <div class="item-cod">{{ $detalle['codigo'] ?? '-' }}</div>
+            <div class="item-precio">{{ number_format($precioFinalUnitario, 2) }}</div>
+            <div class="item-total">{{ number_format($totalFinalLinea, 2) }}</div>
         </div>
-        <div class="item-descripcion">{{ strtoupper($detalle['descripcion'] ?? 'POLO BASICO COLOR SMALL') }}</div>
+        <div class="item-descripcion">{{ strtoupper($detalle['descripcion'] ?? '') }}</div>
+        <div class="item-tax">{{ $igvTexto }}</div>
     @empty
         <div class="item">
-            <div style="width: 100%; text-align: center;">Sin items</div>
+            <div style="width:100%;text-align:center;">Sin items</div>
         </div>
     @endforelse
 </div>
