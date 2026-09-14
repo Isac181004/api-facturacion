ce505a0f063f1f66ace52c2262e3b981e94a12d6
Isac181004 <isacvasquez017@gmail.com>
2026-09-14 12:21:46 -0500

    feat: register integration API middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'integration' => \App\Http\Middleware\AuthenticateIntegration::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
